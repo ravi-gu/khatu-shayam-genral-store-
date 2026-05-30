4420713b86141229c08bd410f4c69638eaab966b
ravi-gu <153809488+ravi-gu@users.noreply.github.com>
2026-05-30 12:30:39 +0530

    Delete sw.js

--- sw.js
+++
@@ -1,7 +0,0 @@
-const CACHE='sks-store-real-login-v2';
-const FILES=['./','./index.html','./style.css','./app.js','./manifest.json','./icon-192.png','./icon-512.png'];
-self.addEventListener('install',e=>{self.skipWaiting();});
-self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>caches.delete(k)))));self.clients.claim();});
-self.addEventListener('fetch',e=>{
-  e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)));
-});
